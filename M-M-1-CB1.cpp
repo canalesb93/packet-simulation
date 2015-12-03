059e8662f31b26d8f2aef98c1642991f813fca97
Ricardo Canales <canalesb93@gmail.com>
2015-12-02 20:10:38 -0600

    Adds files

--- M-M-1-CB1.cpp
+++ M-M-1-CB1.cpp
@@ -9,23 +9,60 @@
 //=  History: ITESM (14/03/2010) - cbarbaji                                    =
 //==============================================================================
 //----- Includes ---------------------------------------------------------------
-#include <stdio.h>              
-#include <stdlib.h>             
-#include <math.h>               
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+// #include <string>
+#include <cctype>
+#include <stack>
+#include <queue>
+#include <list>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <cmath>
+#include <bitset>
+#include <utility>
+#include <set>
+#include <numeric>
+#include <ctime>
+#include <fstream>
+
+
+using namespace std;
 
 //----- Constantes --------------------------------------------------------------
-#define SIM_TIME   1.0e3        // Tiempo de simulacion
-#define LAMBDA     7            // Razon de llegada de mensajes
-#define MIU        8            // Razon de servicio de mensajes
+#define SIM_TIME   1          // Tiempo de simulacion
+#define LAMBDA     2              // Razon de llegada de mensajes
+#define MIU        0.000222       // Razon de servicio de mensajes
+#define K          5              // Cantidad de usuarios maximos
+#define N          15             // Cantidad de usuarios maximos
+#define MTU        1500           // Tamaño máximo de paquete
+#define R          0.04166666667  // Tiempo por cliente por marco
+
+
+struct client {
+  unsigned int id;
+  double arrival;
+  double departure;
+  int current_frame;
+  client(unsigned int i, double a) : id(i), arrival(a), departure(-1.0), current_frame(0) {}
+};
 
 //----- Prototipos de Funciones --------------------------------------------------
 double expVA(double x);        // Generar variable aleatoria exponencial con media x
 
 //===== Main program ===========================================================
-int main(void)
+int main()
 {
-  double   end_time = SIM_TIME; // Tiempo total a simular
-  double   Ta = 1.0/LAMBDA;     // Tiempo promedio de llegada entre mensajes
+  double   end_time = SIM_TIME; // Tiempo total a simular segundos
+
+  double   Ta = 1.0/LAMBDA;     // Tiempo promedio de llegada clientes
   double   Ts = 1.0/MIU;        // Tiempo promedio de servicio por mensaje
 
   double   time = 0.0;          // Tiempo actual de simulacion
@@ -44,49 +81,84 @@ int main(void)
   double   u;                   // Utilizacion
   double   l;                   // Media de paquetes en sistema
   double   w;                   // Media de tiempo de residencia  
-   
+
+  unsigned int k = 0;
+
+  
+  vector<int> frames;
+  queue<client> clients;
+  
+  // Abrir archivos
+  // Extrae los frame
+  ifstream framesFile;
+  framesFile.open ("jurassic.txt");
+  while(!framesFile.eof()){
+    int frame;
+    framesFile >> frame;
+    frames.push_back(frame);
+  }
+  framesFile.close();
+
+  // Extraer delays
+  ifstream delaysFile;
+  delaysFile.open ("amazon_delays.txt");
+  
+  delaysFile.close();
+
+  // for(int i = 0; i < frames.size(); i++){
+  //   printf("%d\n", frames[i]);
+  // }
+
+
   // Ciclo principal
   while (time < end_time)
   {
-    if (tarr < tdep)                // *** Evento (llegada) ***
+    // cout << time << " " << tarr << endl;
+
+    if(time <= 0.49 && time >= 0.5) cout << time << endl;
+    if (time == tarr)                // *** Evento (llegada) de cliente ***
     {
-      time = tarr;
-      s = s + n * (time - tn);  // actualiza area bajo curva de s
-      n++;
+
+      clients.push(client(id, tarr));
+
+      tarr += Ta;                 // se aumenta por el promedio de llegadas 
+      printf("%.3f\n", tarr);
+
+      //s = s + n * (time - tn);  // actualiza area bajo curva de s
+      k++;
       id++;   
       tn = time;                // tn = tiepo de ultimo evento para el siguiente evento
-      printf("Llegada de mensaje %u en T= %.3f sec --- Paquetes en Concentrador: %u\n",id,time,n);
-      tarr = time + expVA(Ta);
-      if (n == 1)
-      {
-        tb = time;              // tb= incio de tiempo de servicio de ultimo paquete en fila
-        tdep = time + expVA(Ts);
-      }
+      printf("Llegada de un cliente %u en T= %.3f sec --- Paquetes en Concentrador: %u\n",id,time,n);
+      // tarr = time + expVA(Ta);
+
     }
-    else                        // *** Evento (salida) ***
-    {
-      time = tdep;
-      s = s + n * (time - tn);  // actualiza area bajo curva de s
-      n--;
-      tn = time;                // tn = tiempo de ultimo evento para el siguiente evento
-      id2++;
-      printf("Termina mensaje %u en T= %.3f sec --- Paquetes en Concentrador: %u\n",id2,time,n);
-      c++;                      // Incrementa numero de servicios completos
-      if (n > 0)
-        tdep = time + expVA(Ts);
-      else
-      {
-        tdep = SIM_TIME;
-        b = b + time - tb;      // Actualizar tiempo de servicio
-      }
-    }
+    // else                        // *** Evento (salida) ***
+    // {
+    //   time = tdep;
+    //   //s = s + n * (time - tn);  // actualiza area bajo curva de s
+    //   n--;
+    //   tn = time;                // tn = tiempo de ultimo evento para el siguiente evento
+    //   id2++;
+    //   printf("Termina mensaje %u en T= %.3f sec --- Paquetes en Concentrador: %u\n",id2,time,n);
+    //   c++;                      // Incrementa numero de servicios completos
+    //   if (n > 0)
+    //     tdep = time + expVA(Ts);
+    //   else
+    //   {
+    //     tdep = SIM_TIME;
+    //     b = b + time - tb;      // Actualizar tiempo de servicio
+    //   }
+    // }
+
+    //printf("%.3f\n", time);
+    time += 0.001;
   }
 
   // Procesa Resultados
-  x = c / time;   // Procesa throughput
-  u = b / time;   // Procesa utilizacion del concentrador
-  l = s / time;   // Computa numero promedio de paquetes en sistema
-  w = l / x;      // Procesa tiempo promedio de estancia en sistema
+  // x = c / time;   // Procesa throughput
+  // u = b / time;   // Procesa utilizacion del concentrador
+  // l = s / time;   // Computa numero promedio de paquetes en sistema
+  // w = l / x;      // Procesa tiempo promedio de estancia en sistema
 
   // Imprime resultados
   printf("=============================================================== \n");
@@ -95,8 +167,8 @@ int main(void)
   printf("=  Tiempo total simulado                    = %.2f s  \n", end_time);
   printf("=============================================================== \n");
   printf("=  Parametros de sistema:                                       \n");
-  printf("=    Razon de llegada de paquetes           = %.2f paq/s \n",1.0/Ta);
-  printf("=    Razon de servicio de los paquetes      = %.2f paq/s \n",1.0/Ts);
+  printf("=    Razon de llegada de paquetes           = %.6f paq/s \n",1.0/Ta);
+  printf("=    Razon de servicio de los paquetes      = %.6f paq/s \n",1.0/Ts);
   printf("=============================================================== \n");
   printf("=  Resultados:                                                  \n");
   printf("=    Numero de paquetes atendidos           = %d paq         \n", c);
@@ -105,6 +177,10 @@ int main(void)
   printf("=    Numero promedio de paquetes en sistema = %.2f paq       \n", l);
   printf("=    Tiempo promedio de estancia en sistema = %.2f s         \n", w);
   printf("=============================================================== \n");
+
+  delaysFile.close();
+
+  return 0;
 }
 
 //==============================================================================
